76214d9a341ebbcb5bed318c5046c317336a614a
Victor <vlougongs@gmail.com>
2024-02-15 23:13:10 +0000

    Implemented FormContext + Removed Restrict Mode!

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { FormProvider } from './context/FormContext.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -8,11 +9,11 @@ import 'bootstrap/dist/js/bootstrap';
 import './assets/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <AuthProvider>
+  <Router>
+    <AuthProvider>
+      <FormProvider>
         <App />
-      </AuthProvider>
-    </Router>
-  </React.StrictMode>
+      </FormProvider>
+    </AuthProvider>
+  </Router>
 );
