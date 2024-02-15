04959cc47758a0898977d8c0d2f09430035dc3ce
Victor <vlougongs@gmail.com>
2024-02-15 23:18:00 +0000

    Temp Change & Icon Changed!

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from 'react-router-dom';
-// import Navbar from '../ui/Navbar';
+import Navbar from '../ui/Navbar';
 import useAuthContext from '../../hooks/useAuthContext';
 
 export default function AuthLayout() {
@@ -8,13 +8,13 @@ export default function AuthLayout() {
     return <Navigate to="/login" />;
   }
   return (
-    // <div className="flex flex-col h-screen bg-slate-100">
-    //   <Navbar />
-    //   <div className="h-full py-10">
-    //     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className="flex flex-col h-screen bg-slate-100">
+      <Navbar />
+      <div className="h-full py-10">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <Outlet />
-    //     </div>
-    //   </div>
-    // </div>
+        </div>
+      </div>
+    </div>
   );
 }
