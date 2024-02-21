7ec3dbbb3adf7d18240b686c648836d0f209495a
Victor <vlougongs@gmail.com>
2024-02-21 19:20:13 +0000

    Implemented Search Box on Contact List!

--- src/pages/lists/ContactList.jsx
+++ src/pages/lists/ContactList.jsx
@@ -7,6 +7,7 @@ import '../../assets/pages/lists/ContactList.css';
 
 export default function ContactList() {
     const [contacts, setContacts] = useState([]);
+    const [curretnContacts, setCurrentContacts] = useState([]);
     const { getBeneficiaryContacts, getContactsBeneficiary, loading } = useAuthContext();
     const beneficiaryID = useParams();
 
@@ -33,6 +34,7 @@ export default function ContactList() {
                     }
 
                     setContacts(contactObject);
+                    setCurrentContacts(contactObject);
                 }
                 return
             }
@@ -41,13 +43,36 @@ export default function ContactList() {
 
             if (getResponse.data.status && getResponse.data.status === 'success') {
                 setContacts(getResponse.data.data);
+                setCurrentContacts(getResponse.data.data);
             }
         }
         setGetResponse();
     }, []);
 
+    const handleChange = (element) => {
+        const nameRegExp = new RegExp(element.target.value, 'ig');
+
+        if (beneficiaryID.id) {
+            setCurrentContacts(contacts.filter(contact => contact.contact_id.name.match(nameRegExp)));
+
+            return
+        }
+
+        setCurrentContacts(contacts.filter(contact => contact.beneficiary_id.name.match(nameRegExp)));
+    };
+
     return (
         <div id="contactList" className="container-fluid">
+
+            <div id='searchBox' className="input-group mb-4">
+                <span className="input-group-text" id="searchBoxLupe">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"></path>
+                    </svg>
+                </span>
+                <input type="text" className="form-control" name='search' placeholder={'Buscar Contactos' + (beneficiaryID.id ? '' : ' por Beneficiario') + '...'} aria-describedby="searchBoxLupe" onChange={handleChange} disabled={loading} />
+            </div>
+
             <div className='table-responsive'>
                 <table id='contactTable' className="table table-striped table-hover">
                     <thead>
@@ -60,7 +85,7 @@ export default function ContactList() {
 
                     <tbody>
                         {!loading &&
-                            <TableRows columns={3} list={'contact'} dataArray={contacts} arrayHandler={setContacts} />
+                            <TableRows columns={3} list={'contact'} dataArray={curretnContacts} arrayHandler={setContacts} />
                         }
                     </tbody>
                 </table>
