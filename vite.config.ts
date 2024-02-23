ad9cda6aec32a1732dd6fa05c737b0e6f5102e67
Victor <vlougongs@gmail.com>
2024-02-23 18:57:34 +0000

    Added Chunk Size Limit!

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base: "/",
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 1600, },
   server: { port: 3000 }
 })
