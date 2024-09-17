86508bc4b307c3a446ef53ecf964492ae34c001e
Vlougon <vlougongs@gmailcom>
2024-09-17 17:53:49 +0100

    Fix: Vite Config Duplicated Values!

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 1600, },
+  build: { chunkSizeWarningLimit: 2500, },
   server: {
     port: 3000, // Port where it will listen
     // TODO: Comment/Remove when Finished Testing.
     watch: { usePolling: true } // Usefull for Locally Testing: Auto-refresh the page when ever a change is made and save.
   }
-  build: { chunkSizeWarningLimit: 2500, },
-  server: { port: 3000 }
 })
