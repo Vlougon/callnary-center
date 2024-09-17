5a77f14bae7fbcff5029f5cbe5fa67d79f1ac491
Vlougon <vlougongs@gmail.com>
2024-09-17 18:09:01 +0100

    Test: Solved Bad Commit Upload!

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   build: { chunkSizeWarningLimit: 2500, },
   server: {
     port: 3000, // Port where it will listen
-    
     // TODO: Comment/Remove when Finished Testing.
     watch: { usePolling: true } // Usefull for Locally Testing: Auto-refresh the page when ever a change is made and save.
   }
