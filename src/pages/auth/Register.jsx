a738ddd0d3d04befbcc75009b2644a4d5ae5f070
Vlougon <vlougongs@gmail.com>
2024-10-14 20:35:18 +0100

    Update: Added Validation into Input Fields!

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -10,11 +10,44 @@ export default function Register() {
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [serialCode, setSerialCode] = useState('');
-  const { login, errors, loading } = useAuthContext();
+  const { register, errors, loading } = useAuthContext();
 
-  const handleLogin = async (e) => {
+  const handleRegisterInput = (element) => {
+    element.target.className = 'form-control';
+    element.target.getAttribute('id').match(/password/)
+      ? element.target.nextElementSibling.nextElementSibling.className = 'invalid-feedback'
+      : element.target.nextElementSibling.className = 'invalid-feedback';
+  }
+
+  const handleRegister = async (e) => {
     e.preventDefault();
-    login({ email, password });
+    let failedChecking = false;
+
+    if (!password.match(/^(?=.{10,})\S*$/) || password.match(/^(?=\s*$)/)) {
+      document.querySelector('#password').className += ' is-invalid';
+      document.querySelector('#serial_code').nextElementSibling.nextElementSibling.className += ' d-block';
+      failedChecking = true;
+    }
+
+    if (!passwordConfirmation.match(/^(?=.{10,})\S*$/) || passwordConfirmation.match(/^(?=\s*$)/) ||
+      password !== passwordConfirmation
+    ) {
+      document.querySelector('#password_confirmation').className += ' is-invalid';
+      document.querySelector('#serial_code').nextElementSibling.nextElementSibling.className += ' d-block';
+      failedChecking = true;
+    }
+
+    if (serialCode.length != 32) {
+      document.querySelector('#serial_code').className += ' is-invalid';
+      document.querySelector('#serial_code').nextElementSibling.className += ' d-block';
+      failedChecking = true;
+    }
+
+    if (failedChecking) {
+      return;
+    }
+
+    register({ email, password, passwordConfirmation, serialCode });
   };
 
   return (
@@ -27,7 +60,7 @@ export default function Register() {
         </div>
 
         <div className="row">
-          <form method="POST" onSubmit={handleLogin}>
+          <form method="POST" onSubmit={handleRegister}>
 
             <div className='col-12 text-sm-start mb-4'>
               <label htmlFor="email" className="form-label">
@@ -46,8 +79,11 @@ export default function Register() {
                 </label>
               </div>
               <div className="col-12 input-group">
-                <input id="password" name="password" type="password" placeholder='Contraseña' autoComplete="current-password" className='form-control' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input id="password" name="password" type="password" placeholder='Contraseña' autoComplete="current-password" className='form-control' value={password} onChange={(e) => setPassword(e.target.value)} onClick={handleRegisterInput} />
                 <PasswordIcon />
+                <div className="invalid-feedback">
+                  <p>¡La Contraseña Debe tener 10 carácteres como mínimo (No debe tener espacios)!</p>
+                </div>
               </div>
               <p className='text-danger'>
                 {errors ? errors.password : false && (<span className="text-danger">{errors.password[0]}</span>)}
@@ -60,9 +96,12 @@ export default function Register() {
                   Confirmar Contraseña:
                 </label>
               </div>
-              <div className="col-12 input-group">
-                <input id="password_confirmation" name="password_confirmation" type="password" placeholder='Confirmar Contraseña' autoComplete="current-password" className='form-control' value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} />
+              <div className="col-12 input-group has-validation">
+                <input id="password_confirmation" name="password_confirmation" type="password" placeholder='Confirmar Contraseña' autoComplete="current-password" className='form-control' value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} onClick={handleRegisterInput} />
                 <PasswordIcon />
+                <div className="invalid-feedback">
+                  <p>¡Las Contraseñas Deben ser iguales!</p>
+                </div>
               </div>
               <p className='text-danger'>
                 {errors ? errors.password_confirmation : false && (<span className="text-danger">{errors.password_confirmation[0]}</span>)}
@@ -73,7 +112,10 @@ export default function Register() {
               <label htmlFor="serial_code" className="form-label">
                 Código del Centro:
               </label>
-              <input id="serial_code" name="serial_code" type="text" placeholder='Código del Centro' className='form-control' value={serialCode} onChange={(e) => setSerialCode(e.target.value)} />
+              <input id="serial_code" name="serial_code" type="text" placeholder='Código del Centro' className='form-control' value={serialCode} onChange={(e) => setSerialCode(e.target.value)} onClick={handleRegisterInput} />
+              <div className="invalid-feedback">
+                <p>¡Introduzca un Código del Centro Válido!</p>
+              </div>
               <p className='text-danger'>
                 {errors ? errors.serial_code : false && (<span className="text-danger">{errors.serial_code[0]}</span>)}
               </p>
